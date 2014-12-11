8e77ecebfaf96fa950761200559e2b98f92501ba
Stefan Thomas <justmoon@members.fsf.org>
2014-12-11 02:33:08 -0800

    [FEATURE] Strongly connected components.

--- js/analyzer.js
+++ js/analyzer.js
@@ -6,19 +6,31 @@ var Analyzer = function (state) {
 };
 
 Analyzer.colors = [
-  '#4D4D4D', // gray
-  '#5DA5DA', // blue
-  '#FAA43A', // orange
   '#60BD68', // green
+  '#FAA43A', // orange
+  '#5DA5DA', // blue
   '#F17CB0', // pink
   '#B2912F', // brown
   '#B276B2', // purple
   '#DECF3F', // yellow
   '#F15854', // red
+  '#4D4D4D', // gray
 ];
 
 Analyzer.prototype.update = function () {
-  this.state.current.nodes.forEach(function (node, i) {
-    node.color = Analyzer.colors[i % Analyzer.colors.length];
+  // Convert to adjacency list
+  // TODO May be more efficient to rewrite the Tarjan's algorithm
+  //      to expect a list in the format that we actually use.
+  var adjList = this.state.current.nodes.map(function (node) {
+    return (node.advisors || []).map(function (node) {
+      return this.state.current.nodes.indexOf(node);
+    });
+  });
+  var scc = Graph.stronglyConnectedComponents(adjList);
+  scc.components.forEach(function (component, i) {
+    component.forEach(function (nodeId) {
+      var node = this.state.current.nodes[nodeId];
+      node.color = Analyzer.colors[i % Analyzer.colors.length];
+    });
   });
 };
